45439748be93e47e5fc6acc9d758604638b967b1
emmanuelannim <emmanuel.annim@hva.nl>
2020-12-16 13:15:45 +0100

    Story9: final changes

--- funda/load_data.py
+++ funda/load_data.py
@@ -42,8 +42,7 @@ class DataLoader(object):
     def load_cbs_postcodes(self):
         full_path = os.path.join(self.base_folder, 'data/raw/pc6-gwb2020.csv')
         data = pd.read_csv(full_path, sep=";")
-        return data
-    
+        return data    
     def load_funda_images(self):
         # here the code to load raw data from base folder is implemented
         raise NotImplementedError('Not yet implemented')

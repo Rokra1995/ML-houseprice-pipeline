ccd20f8b393e50da2ac6c1f00bd7a0456f52e2f0
Felicia Betten <feliciabetten.fb@gmail.com>
2021-01-06 11:27:06 +0100

    Final clean_data.py

--- funda/clean_data.py
+++ funda/clean_data.py
@@ -1,4 +1,4 @@
-'''This module contains classes to clean the data 
+'''This module contains the class to clean all the data 
 ''' 
 
 import pandas as pd
@@ -18,11 +18,12 @@ class DataCleaner(object):
         # m³ removed from parcelsurface  
         data['parcelsurface'] = data['parcelsurface'].str.replace(r'\D', '').astype(int)
         # Remove \r \n from housetype
-        data['housetype'] = df_funda_2020['housetype'].str.rstrip('\r\n')
+        data['housetype'] = data['housetype'].str.rstrip('\r\n')
         # Replace 0 in Garden_binary with NaN
         data['garden_binary'] = data['garden_binary'].replace(0, np.nan)      
     
-        def calculate_mean_yearofbuilding_funda_2020(self, date):
+        @staticmethod
+        def calculate_mean_yearofbuilding_funda_2020(date):
             date = date.replace('After ', '') # replace 'After ' with empty
             date = date.replace('Before ', '') # replace 'Before ' with empty
             if "-" in date: # check whether there is a '-' in the column
@@ -33,49 +34,50 @@ class DataCleaner(object):
                 return int(date) # if not, return the only date
 
             # apply function to each row of the column 
-        data['yearofbuilding'] = data['yearofbuilding'].apply(lambda date: self.calculate_mean_yearofbuilding_funda_2020(date))
+        data['yearofbuilding'] = data['yearofbuilding'].apply(lambda date: calculate_mean_yearofbuilding_funda_2020(date))
         print("Funda data 2020 cleaned")
         return data
     
     # © Baris Orman
     @staticmethod
     def cleaned_funda_2018(data):
-      #Renaming the columns to english
-      data = data.fillna(0).rename(columns={'publicatieDatum':'publicationDate','postcode':'zipcode', 'koopPrijs':'sellingPrice',\
+        #Renaming the columns to english
+        data = data.fillna(0).rename(columns={'publicatieDatum':'publicationDate','postcode':'zipcode', 'koopPrijs':'sellingPrice',\
           'volledigeOmschrijving':'fullDescription','soortWoning':'houseType','categorieObject':'categoryObject', 'bouwjaar':'yearOfBuilding', \
           'indTuin':'garden','perceelOppervlakte':'parcelSurface','aantalKamers':'numberRooms','aantalBadkamers':'numberBathrooms',   'energielabelKlasse':'energylabelClass',\
           'oppervlakte':'surface','datum_ondertekening':'sellingDate'}).drop(['globalId', 'globalId.1','kantoor_naam_MD5hash'], axis=1)
 
-      #Changing dataypes for publication date and selling date
-      data['publicationDate'] = pd.to_datetime(data['publicationDate'])
-      data['sellingDate'] = pd.to_datetime(data['sellingDate'])
+        #Changing dataypes for publication date and selling date
+        data['publicationDate'] = pd.to_datetime(data['publicationDate'])
+        data['sellingDate'] = pd.to_datetime(data['sellingDate'])
 
-      #HOUSETYPE AND CATEGORYOBJECT: SEPERATE THE VARIABALES WITH COMMA'S AND REMOVE THE BRACKETS
-      data['houseType'] = data['houseType'].str.replace('<', "").str.replace('{', "").str.replace('}', "").str.replace('>', "")
-      data['categoryObject'] = data['categoryObject'].str.replace('<', "").str.replace('{', "").str.replace('}', "").str.replace('>', "")
-      data['fullDescription'] = data['fullDescription'].str.replace("\n", "")
+        #HOUSETYPE AND CATEGORYOBJECT: SEPERATE THE VARIABALES WITH COMMA'S AND REMOVE THE BRACKETS
+        data['houseType'] = data['houseType'].str.replace('<', "").str.replace('{', "").str.replace('}', "").str.replace('>', "")
+        data['categoryObject'] = data['categoryObject'].str.replace('<', "").str.replace('{', "").str.replace('}', "").str.replace('>', "")
+        data['fullDescription'] = data['fullDescription'].str.replace("\n", "")
 
-      #Calculation of sellingtime and adding the column
-      data['sellingTime'] = pd.to_datetime(data['sellingDate']) - pd.to_datetime(data['publicationDate'])
-      data['sellingTime'] = data['sellingTime'].apply(lambda x: int(x.days))
+        #Calculation of sellingtime and adding the column
+        data['sellingTime'] = pd.to_datetime(data['sellingDate']) - pd.to_datetime(data['publicationDate'])
+        data['sellingTime'] = data['sellingTime'].apply(lambda x: int(x.days))
 
-      #Replace the 0 in Parcelsruface with NaN
-      data['parcelSurface'] = data['parcelSurface'].replace(0.0, np.nan)
+        #Replace the 0 in Parcelsruface with NaN
+        data['parcelSurface'] = data['parcelSurface'].replace(0.0, np.nan)
 
-          #CALCULATE THE MEAN OF VERY OLD YEAR OF BUILDINGS
-          def mean_yearofBuilding_funda_2018(date):
-              date = date.replace('<{Voor}> ', '')
-              date = date.replace('<{Na}> ', '')
-              if '-' in date:
-                  date = date.split("-")
-                  date = (int(date[0]) + int(date[1])) / 2
-                  return(int(date))
-              else:
-                  return int(date)
+        #CALCULATE THE MEAN OF VERY OLD YEAR OF BUILDINGS
+        @staticmethod
+        def mean_yearofBuilding_funda_2018(date):
+            date = date.replace('<{Voor}> ', '')
+            date = date.replace('<{Na}> ', '')
+            if '-' in date:
+                date = date.split("-")
+                date = (int(date[0]) + int(date[1])) / 2
+                return(int(date))
+            else:
+                return int(date)
 
-      data['yearOfBuilding'] = data['yearOfBuilding'].apply(lambda date: mean_yearofBuilding_funda_2018(date))
-      print("Funda data 2018 cleaned")
-      return data
+        data['yearOfBuilding'] = data['yearOfBuilding'].apply(lambda date: mean_yearofBuilding_funda_2018(date))
+        print("Funda data 2018 cleaned")
+        return data
 
     # © Robin Kratschmayr
     @staticmethod
@@ -94,9 +96,6 @@ class DataCleaner(object):
             'zipcode_broker':'string',
             'description_broker': 'string',
             'score_broker': 'float64',
-            'number_reviews_broker': 'int64',
-            'number_houses_for_sale_offered': 'int64',
-            'number_houses_sold_last_12_months': 'int64'}
             'number_reviews_broker': 'Float64',# has to be converted to float since the column contains NaN values that are not convertible to int
             'number_houses_for_sale_offered': 'Float64',
             'number_houses_sold_last_12_months': 'Float64',

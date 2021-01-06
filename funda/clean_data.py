173db7bac07de29c0564ae603307061f194640d5
Felicia Betten <73534546+feliciabetten@users.noreply.github.com>
2021-01-06 09:15:19 +0100

    Update clean_data.py

--- funda/clean_data.py
+++ funda/clean_data.py
@@ -22,7 +22,6 @@ class DataCleaner(object):
         # Replace 0 in Garden_binary with NaN
         data['garden_binary'] = data['garden_binary'].replace(0, np.nan)      
     
-        # © Felicia Betten
         def calculate_mean_yearofbuilding_funda_2020(self, date):
             date = date.replace('After ', '') # replace 'After ' with empty
             date = date.replace('Before ', '') # replace 'Before ' with empty
@@ -37,47 +36,46 @@ class DataCleaner(object):
         data['yearofbuilding'] = data['yearofbuilding'].apply(lambda date: self.calculate_mean_yearofbuilding_funda_2020(date))
         print("Funda data 2020 cleaned")
         return data
-      
+    
+    # © Baris Orman
+    @staticmethod
     def cleaned_funda_2018(data):
       #Renaming the columns to english
-      funda_2018 = data
-      funda_2018 = funda_2018.fillna(0).rename(columns={'publicatieDatum':'publicationDate','postcode':'zipcode', 'koopPrijs':'sellingPrice',\
+      data = data.fillna(0).rename(columns={'publicatieDatum':'publicationDate','postcode':'zipcode', 'koopPrijs':'sellingPrice',\
           'volledigeOmschrijving':'fullDescription','soortWoning':'houseType','categorieObject':'categoryObject', 'bouwjaar':'yearOfBuilding', \
           'indTuin':'garden','perceelOppervlakte':'parcelSurface','aantalKamers':'numberRooms','aantalBadkamers':'numberBathrooms',   'energielabelKlasse':'energylabelClass',\
           'oppervlakte':'surface','datum_ondertekening':'sellingDate'}).drop(['globalId', 'globalId.1','kantoor_naam_MD5hash'], axis=1)
 
       #Changing dataypes for publication date and selling date
-      funda_2018['publicationDate'] = pd.to_datetime(funda_2018['publicationDate'])
-      funda_2018['sellingDate'] = pd.to_datetime(funda_2018['sellingDate'])
+      data['publicationDate'] = pd.to_datetime(data['publicationDate'])
+      data['sellingDate'] = pd.to_datetime(data['sellingDate'])
 
       #HOUSETYPE AND CATEGORYOBJECT: SEPERATE THE VARIABALES WITH COMMA'S AND REMOVE THE BRACKETS
-      funda_2018['houseType'] = funda_2018['houseType'].str.replace('<', "").str.replace('{', "").str.replace('}', "").str.replace('>', "")
-      funda_2018['categoryObject'] = funda_2018['categoryObject'].str.replace('<', "").str.replace('{', "").str.replace('}', "").str.replace('>', "")
-      funda_2018['fullDescription'] = funda_2018['fullDescription'].str.replace("\n", "")
+      data['houseType'] = data['houseType'].str.replace('<', "").str.replace('{', "").str.replace('}', "").str.replace('>', "")
+      data['categoryObject'] = data['categoryObject'].str.replace('<', "").str.replace('{', "").str.replace('}', "").str.replace('>', "")
+      data['fullDescription'] = data['fullDescription'].str.replace("\n", "")
 
       #Calculation of sellingtime and adding the column
-      funda_2018['sellingTime'] = pd.to_datetime(funda_2018['sellingDate']) - pd.to_datetime(funda_2018['publicationDate'])
-      funda_2018['sellingTime'] = funda_2018['sellingTime'].apply(lambda x: int(x.days))
+      data['sellingTime'] = pd.to_datetime(data['sellingDate']) - pd.to_datetime(data['publicationDate'])
+      data['sellingTime'] = data['sellingTime'].apply(lambda x: int(x.days))
 
       #Replace the 0 in Parcelsruface with NaN
-      funda_2018['parcelSurface'] = funda_2018['parcelSurface'].replace(0.0, np.nan)
+      data['parcelSurface'] = data['parcelSurface'].replace(0.0, np.nan)
 
-      #CALCULATE THE MEAN OF VERY OLD YEAR OF BUILDINGS
-      def mean_yearofBuilding_funda_2018(date):
-          date = date.replace('<{Voor}> ', '')
-          date = date.replace('<{Na}> ', '')
-          if '-' in date:
-              date = date.split("-")
-              date = (int(date[0]) + int(date[1])) / 2
-              return(int(date))
-          else:
-              return int(date)
+          #CALCULATE THE MEAN OF VERY OLD YEAR OF BUILDINGS
+          def mean_yearofBuilding_funda_2018(date):
+              date = date.replace('<{Voor}> ', '')
+              date = date.replace('<{Na}> ', '')
+              if '-' in date:
+                  date = date.split("-")
+                  date = (int(date[0]) + int(date[1])) / 2
+                  return(int(date))
+              else:
+                  return int(date)
 
-
-      funda_2018['yearOfBuilding'] = funda_2018['yearOfBuilding'].apply(lambda date: mean_yearofBuilding_funda_2018(date))
-      datatypes_funda_2018 = funda_2018.dtypes
-
-      return funda_2018
+      data['yearOfBuilding'] = data['yearOfBuilding'].apply(lambda date: mean_yearofBuilding_funda_2018(date))
+      print("Funda data 2018 cleaned")
+      return data
 
     # © Robin Kratschmayr
     @staticmethod
@@ -140,4 +138,3 @@ class DataCleaner(object):
         #Translate Dutch Headers to English Headers
         data = data.rename(columns={'Onderwijsvolgend': 'Educational', 'KenmerkenArbeid': 'Characteristics Labor', 'Uitkering': 'Payment', 'IngeschrevenUWVWerkbedrijf':'RegisteredUWVWerkbedrijf', 'RegioS': 'Municipalitycode', 'Perioden': 'Periods', 'Jongeren15Tot27Jaar_1':'Youth15To27Year' })
         return data
-

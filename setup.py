c05500b876408035d3dc220f44ff51ba14d6597e
Rokra1995 <rkratschmayr@me.com>
2021-01-24 16:31:44 +0100

    Story_41_final

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='funda',
-    version='0.1.1',
-    description='Example package for ML pipeline',
+    version='1.0',
+    description='ML Pipeline to train Random Forest and Neural Network for houespriceprediction based on funda dataset',
     packages=find_packages(include=['funda']),
     install_requires=[
         "pandas",
         "numpy",
-        "plotnine",
         "scikit-learn",
         "matplotlib",
         'pyarrow',
-        'geopandas'
+        'geopandas',
+        'descartes'
     ],
     python_requires="==3.8.5"
 )
